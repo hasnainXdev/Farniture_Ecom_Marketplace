bdd5797dc8be1a9648d722f23b143934a937c3bc
Muhammad Hasnain <tryfrelancer23@gmail.com>
2025-11-17 00:43:52 +0500

    fix type errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-//@ts-ignore
+//@ts-expect-error
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/FooterSection";
